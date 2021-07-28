242eb609cd034890bb5ad8f21f353debf04affcf
Michael Ducharm <ducharmdev@gmail.com>
2021-07-28 17:11:29 -0400

    account for images when adding code snippets to md

--- combine_snippets.py
+++ combine_snippets.py
@@ -7,6 +7,9 @@ def create_snippet(s) -> str:
         "```\n"
     ])
 
+def create_image_link(file):
+    return f"![]({os.path.relpath(file)})"
+
 def section_template(section_name, snippets) -> str:
     return f"""
 ## {section_name}
@@ -18,9 +21,14 @@ def create_section(d) -> str:
     snippets = []
     files = os.scandir(d.path)
     for file in files:
-        with open(file, encoding="utf8") as f:
-            snippet = create_snippet(f.read())
+        is_image = file.name.lower().endswith(('.png', '.jpg', '.jpeg', '.tiff', '.bmp', '.gif'))
+        if is_image:
+            snippet = create_image_link(file)
             snippets.append(snippet)
+        else:
+            with open(file, encoding="utf8") as f:
+                snippet = create_snippet(f.read())
+                snippets.append(snippet)
 
     combined_snippets = "\n".join(snippets)
 
